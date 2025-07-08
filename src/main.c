e789743ca62e270d739f4a75f20e62a0382c8dab
Guido <128338493+OneRing2Find@users.noreply.github.com>
2025-07-08 04:13:55 -0400

    Add menu option to display task titles and view full details

--- src/main.c
+++ src/main.c
@@ -6,12 +6,15 @@
 int main() {
     Task tasks[MAX_TASKS];   // Fixed-size array to store tasks
     int task_count = 0;
-    int option = 0;          // User menu choice
+    int option = 0;         // User menu choice
+    int option1 = 0;
 
     while (option != 9) {
         // Simple menu prompt
+        puts("");
         puts("[1] Add Task");
         puts("[2] Delete Task");
+        puts("[3] Show Tasks");
         puts("[9] Exit");
         printf("Choose an option: ");
         scanf("%1d", &option);
@@ -23,6 +26,32 @@ int main() {
                 task_show(tasks, &task_count);
                 break;
             case 2:
+                break;
+            case 3:
+                if (task_count > 0) {
+                    puts("Tasks: ");
+                    for (int i = 0; i < task_count; i++) {
+                        printf("[%d] %s\n", i, tasks[i].title);
+                    }
+                } else {
+                    puts ("There is no task");
+                    break;
+                }
+
+                puts("Choose a task to see details: ");
+                scanf("%d", &option);
+                clear_input_buffer();
+
+                if (option >= 0 && option1 < task_count) {
+                    puts("Task details:");
+                    printf("ID: %d\n", tasks[option].id);
+                    printf("Title: %s\n", tasks[option].title);
+                    printf("Description: %s\n", tasks[option].description);
+                    printf("Status: %d\n", tasks[option].status);
+                    puts("");
+                } else {
+                    puts("Invalid ID. Try again.");
+                }
                 break;
             case 9:
                 printf("Exiting...\n");
